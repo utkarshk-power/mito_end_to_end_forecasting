4ffc36d557f34a8074c6c314d9912f64958b0709
Urkarsh Kulshrestha <urkarsh.kulshrestha@px-01164.local>
2026-02-10 19:38:59 +0900

    Updated data saving directory for proper dvc pull on central side

--- airflow_dags/include/forecasting/src/inference/data_push_sys_scheduler.py
+++ airflow_dags/include/forecasting/src/inference/data_push_sys_scheduler.py
@@ -63,7 +63,7 @@ def push_data_to_dvc(data):
     update_state = new_data_available and enough_time_passed
     #repo_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))) 
     if update_state:
-        repo_root = os.path.dirname(os.path.abspath(__file__))
+        repo_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
         DVC_root = "/opt/mito_end_to_end_forecasting/.venv/bin/dvc"
         output_file = config.get("new_data_params", {}).get("output_file", "latest_site_data.csv")
         subprocess.run(["git", "checkout", "data-snapshots"], cwd=repo_root, check=True)
